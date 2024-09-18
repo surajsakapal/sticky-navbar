84bf7aecc408e0294beaaa42dcf3737046fd05e4
Suraj Sakpal <92380511+surajsakapal@users.noreply.github.com>
2024-09-18 12:15:45 +0530

    Add files via upload

---
+++ sticky-navigation/script.js
@@ -0,0 +1,11 @@
+let nav = document.querySelector('.nav')
+window.addEventListener('scroll',fixnav)
+
+function fixnav(){
+if(window.scrollY > nav.offsetHeight + 150){
+    nav.classList.add('active')
+}
+else{
+    nav.classList.remove('active')
+}
+}
